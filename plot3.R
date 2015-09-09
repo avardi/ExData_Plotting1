311b7324c74b2d1f36cc3a90878fd7e3c8b47936
avardi <ashervardi@gmail.com>
2015-09-09 11:07:19 +0300

    Plotting sub_metering

---
+++ plot3.R
@@ -0,0 +1,20 @@
+DF.row1 <- read.table("../exdata-data-household_power_consumption/household_power_consumption.txt", sep = ";", header = TRUE, nrow = 1)
+nc <- ncol(DF.row1)
+DF.Date <- read.table("../exdata-data-household_power_consumption/household_power_consumption.txt", sep = ";", header = TRUE, as.is = TRUE, 
+                      colClasses = c(NA, rep("NULL", nc - 1)))
+DF.Date <- as.Date(DF.Date[, 1], "%d/%m/%Y")
+n1 <- which.max(DF.Date >= "2007-02-01")
+n2 <- which.max(DF.Date > "2007-02-02")
+
+DF3 <- read.table("../exdata-data-household_power_consumption/household_power_consumption.txt", col.names = names(DF.row1), sep =";", skip = n1, nrows = (n2 - n1), as.is = TRUE)
+
+DF3[ , 1] <- as.Date(DF3$Date, "%d/%m/%Y")
+DF3$Time <- strptime(paste0(DF3$Date, ' ', DF3$Time), '%Y-%m-%d %H:%M:%S')
+with(DF3, plot(Time, Sub_metering_1, type = "n",ylab = "Energu sub metering", xlab = ""))
+lines(DF3$Time, DF3$Sub_metering_1)
+lines(DF3$Time, DF3$Sub_metering_2, col="red")
+lines(DF3$Time, DF3$Sub_metering_3, col="blue")
+legend("topright" , col = c("black", "blue", "red"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty  = 3)
+with(subset(DF3, Sub_metering_1), lines(DF3$Time, DF3$Global_active_power, col="red"))
+dev.copy(png, "plot3.png")
+dev.off()
